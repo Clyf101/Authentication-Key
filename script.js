33b3db9fccd6582a97e0c5627ba5e7640381fe58
Clyford <hclyford@gmail.com>
2023-02-18 06:55:12 -0800

    first commit

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
-// Assignment Code
+
 var generateBtn = document.querySelector("#generate");
+
+// this function to generate the password with a prompt of how long should this password be 
+function generatePassword() {
+  var length = prompt("Choose The length for your password");
+  // invalid input alert to let the user know only numbers is acceptable
+  if (isNaN(length)) {
+    alert("Invalid input. Please enter a number.");
+  }
+  
+  // Validate the user's input
+  if (length < 10 || length > 128) {
+    alert("Password length must be a number between 10 and 128.");
+    return "";
+  }
+
+  var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]\:;?><,./-=";
+  var password = "";
+  for (var i = 0, n = charset.length; i < length; ++i) {
+    password += charset.charAt(Math.floor(Math.random() * n));
+  }
+  return password;
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -7,8 +29,7 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
-// Add event listener to generate button
+// event listener to generate button
 generateBtn.addEventListener("click", writePassword);
