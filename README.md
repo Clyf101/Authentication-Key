dbbf498d471860b01b88cb08d436a33b2d27f95c
Clyford <hclyford@gmail.com>
2023-02-25 06:14:01 -0800

    edit the readme, added a screenshot, use indentation, fix error on the console,

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 ### Description
 Users can generate a key by typing a number to determine the length of the key, any other character input by the user won't be accepted and the user will be alerted to enter a number.
 ### Screenshots
-![Alt text](Keygen.png)
+![Alt text](assets/Keygen.png)
+![Alt text](assets/Screenshot%202023-02-25%20060908.png)
 #### Links
 - https://clyf101.github.io/HWRK3/
 - https://github.com/Clyf101/HWRK3
--- script.js
+++ script.js
@@ -1,29 +1,35 @@
-
+// Select the generate button element
 var generateBtn = document.querySelector("#generate");
 
-// this function to generate the password with a prompt of how long should this password be 
+// Function to generate the password with a prompt to get the length
 function generatePassword() {
-  var length = prompt("Choose The length for your password");
-  // invalid input alert to let the user know only numbers is acceptable
+  // Prompt the user for password length
+  var length = prompt("Choose the length for your password");
+
+  // Check if the input is not a number
   if (isNaN(length)) {
     alert("Invalid input. Please enter a number.");
   }
-  
+
   // Validate the user's input
   if (length < 10 || length > 128) {
     alert("Password length must be a number between 10 and 128.");
     return "";
   }
 
+  // Define the characters that the password can include
   var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]\:;?><,./-=";
   var password = "";
+
+  // Generate the password by randomly selecting characters from the charset string
   for (var i = 0, n = charset.length; i < length; ++i) {
     password += charset.charAt(Math.floor(Math.random() * n));
   }
+
   return password;
 }
 
-// Write password to the #password input
+// Function to write the generated password to the #password textarea
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -31,5 +37,5 @@ function writePassword() {
   passwordText.value = password;
 }
 
-// event listener to generate button
+// Add an event listener to the generate button to call the writePassword function when clicked
 generateBtn.addEventListener("click", writePassword);
